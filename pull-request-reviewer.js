c01a1cc358d1efc9de6b7e3b7c381bea1733a8b0
Christopher Lane <christopher.lane@spidasoftware.com>
2014-06-18 10:52:59 -0400

    The actual final code

--- pull-request-reviewer.js
+++ pull-request-reviewer.js
@@ -1,6 +1,5 @@
-console.time("Total");
 // ==UserScript==
-// @name       Github Pull Request Reviewer by Jeremy Wentworth
+// @name       Github Pull Request Reviewer by SPIDA
 // @namespace  http://spidasoftware.com/
 // @version    0.1
 // @description  Give some order to your pull request
@@ -29,12 +28,10 @@ $("#site-container").bind("DOMSubtreeModified", function() {
 ///////////////////////////////////////////////////////////////
 // Style
 ///////////////////////////////////////////////////////////////
-console.time("Style");
 $(".container").width("93%");
 $("#js-repo-pjax-container").width("80%");
 $(".meta").css("padding","0px 10px");
 $(".file").css("margin-bottom","3px");
-console.timeEnd("Style");
 
 ///////////////////////////////////////////////////////////////
 // DnD Sort
@@ -44,18 +41,15 @@ $("#files").sortable({
     handle: ".handle",
     start: function( event, ui ) {},
     stop: function( event, ui ) {
-        console.time("Event");
         $.jStorage.flush();
         $(".file.js-details-container").each(function(y, elementIterator){
             var updatedTempChild = $(elementIterator).children().first();
             var updatedDataPath = $(updatedTempChild).attr('data-path');
             $.jStorage.set(y, updatedDataPath);
         });
-        console.timeEnd("Event");
     }
 });
 
-console.time("Toggle Data");
 ///////////////////////////////////////////////////////////////
 // Toggle data view for all files
 ///////////////////////////////////////////////////////////////
@@ -80,9 +74,7 @@ $('<a>',{
         $(".data").hide()
     }
 }).insertBefore($(".show-diff-stats"));
-console.timeEnd("Toggle Data");
 
-console.time("Buttons");
 ///////////////////////////////////////////////////////////////
 // Buttons for each file
 ///////////////////////////////////////////////////////////////
@@ -139,37 +131,28 @@ $(".file").each(function(){
     }).appendTo(actionsDiv);
     
 });
-console.timeEnd("Buttons");
 
     //Change "View" button to "Open" and now opens in new tab
-    console.time("View");
     $(".minibutton.tooltipped.tooltipped-s").each(function(i, elementIterator){
             elementIterator.target = "_blank";
     });
-    console.timeEnd("View");
 
 requestAnimationFrame(function() {
-    console.time("View2");
     $(".minibutton.tooltipped.tooltipped-s").each(function(b, elementIterator){
         if(elementIterator.innerText === "View"){
             elementIterator.innerText = "Open";
         }
     });
-    console.timeEnd("View2");
 });
 
     //Repository link doesn't open repository anymore, now opens pull request
-console.timeEnd("Repo Link");
 $(".list-group-item-name").each(function(w, elementIterator){
     var reposLink = $(elementIterator).children().first().next();
     var pullLink = reposLink.next();
     reposLink.attr('href', pullLink.attr('href'));
 });
 
-console.timeEnd("Repo Link");
-
 //File location abbreviated
-    console.time("File Location");
     var arrayOfLocations = [];
 $(".js-selectable-text").each(function(m, elementIterator) {
     var stringLength = elementIterator.innerText.length;        
@@ -181,21 +164,18 @@ $(".js-selectable-text").each(function(m, elementIterator) {
         arrayOfLocations[m] = elementIterator.innerText;
     }
 });
-    console.timeEnd("File Location");
 
 requestAnimationFrame(function() {
-    console.time("File2");
     $(".js-selectable-text").each(function(v, elementIterator) {
         elementIterator.innerText = arrayOfLocations[v];
     });
-    console.timeEnd("File2");
 });
 
 //jStorage
 function getIndex(node){
-    var fileElement2 = $("#files")[0];
-    var childList = fileElement2.childNodes;
-    var lengthOfFilesChildNodes = fileElement2.childNodes.length;
+    var fileElementList = $("#files")[0];
+    var childList = fileElementList.childNodes;
+    var lengthOfFilesChildNodes = fileElementList.childNodes.length;
     var checkForId = false;
     var indexOfMatch = 0;
     var f = 0;
@@ -210,7 +190,6 @@ function getIndex(node){
 }
 
 function jStorage(){
-    console.time("jStorage");
     var fileElement = $("#files")[0];
     $(".file.js-details-container").each(function(outerCounter, outerStorageIteration){
         $(".file.js-details-container").each(function(innerCounter, innerStorageIteration){
@@ -224,7 +203,5 @@ function jStorage(){
         }
     });
 });
-    console.timeEnd("jStorage");
 }
 requestAnimationFrame(jStorage);
-console.timeEnd("Total");
